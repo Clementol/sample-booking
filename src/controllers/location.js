f81e574f7ce8d62ffd3572ad4dd35debf4078a9b
Clementol <clementolaniyan95@gmail.com>
2021-12-23 11:06:29 +0100

    updates

--- src/controllers/location.js
+++ src/controllers/location.js
@@ -4,10 +4,14 @@ const cityLocations = async (req, res) => {
   try {
     const { city } = req.params;
     Location.find({ "city": { $eq: city } }).exec((_, locations) => {
-      if (res) {
+      if (locations) {
         res.status(200).json({ locations });
         return;
+      } else {
+        res.status(400).json({message: `No location found` });
+        return;
       }
+    
     });
   } catch (error) {
     return res.status(400).json({ error });
