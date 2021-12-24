81653869be36885dbc33bd99af899492d195a688
Clementol <clementolaniyan95@gmail.com>
2021-12-24 04:54:24 +0100

    updates

--- src/controllers/booking.js
+++ src/controllers/booking.js
@@ -18,7 +18,7 @@ const createBooking = async (req, res) => {
     //check if user already booked the course
     Booking.find({
       _id: courseId,
-      students: { $eq: { studentId: studentId } },
+      "students.studentId": studentId 
     }).exec((_, booking) => {
       
       if ((booking.length == 1)) {
